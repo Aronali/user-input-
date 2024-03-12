f374cedbfda3d56be647a606dff3b8d6070061e9
Aronali <aronali8728@gmail.com>
2024-03-12 22:57:17 +0300

    simple inputs

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask user for name
+name = input("Enter your name: ")
+
+# Ask  for age
+age = input("Enter your age: ")
+
+# Ask location
+location = input("Enter your location: ")
+
+# the displayed message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
